039bdc7b9ac3944d563100bbf6dbe3533ece27e7
itbetheguy <raheem@iicorp.org>
2026-05-15 15:09:00 -0500

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,10 @@
+{
+  "name": "iicreceipts-form",
+  "version": "1.0.0",
+  "private": true,
+  "description": "Receipt submission form for the IIC Invoice App",
+  "dependencies": {
+    "busboy": "^1.6.0",
+    "nodemailer": "^6.9.13"
+  }
+}
